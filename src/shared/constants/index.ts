753b4791b07cb8dae9df80b4b72d48c4deef8e92
jsh0116 <seunghoon7561@gmail.com>
2026-02-22 18:35:55 +0900

    fix: site url

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,5 +1,4 @@
-export const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ?? "https://xiaohongshu-web.vercel.app";
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const COUNTER_START = 256;
 
